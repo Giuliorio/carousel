dcdae715c889345840a8148b7cf579ace7017d22
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-26 19:19:58 -0700

    Add index tracking data structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './reset.css';
 import './styles.css';
+import useIndex from './useIndex';
+
+const slides = document.querySelectorAll('.slide');
+const { getCurrentIndex, setCurrentIndex, increment, decrement } = useIndex(
+  slides.length - 1
+);
---
+++ src/useIndex.js
@@ -0,0 +1,36 @@
+function useIndex(max) {
+  let currentIndex = 0;
+
+  const getCurrentIndex = () => current;
+
+  const setCurrentIndex = (newIndex) => {
+    if (newIndex >= 0 && newIndex <= max) {
+      currentIndex = newIndex;
+    }
+  };
+
+  const increment = () => {
+    if (currentIndex < max) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+  };
+
+  const decrement = () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = max;
+    }
+  };
+
+  return {
+    getCurrentIndex,
+    setCurrentIndex,
+    increment,
+    decrement,
+  };
+}
+
+export default useIndex;
