497859103cf30ad0dab7b05d593b7f09732907ca
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-27 14:50:35 -0700

    Add next and previous button functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import './styles.css';
 import useIndex from './useIndex';
 
 const slides = document.querySelectorAll('.slide');
+const track = document.querySelector('.track');
 const { getCurrentIndex, setCurrentIndex, increment, decrement } = useIndex(
   slides.length - 1
 );
+
+const previousButton = document.querySelector('.previous');
+const nextButton = document.querySelector('.next');
+
+previousButton.addEventListener('click', handlePrev);
+nextButton.addEventListener('click', handleNext);
+
+function handlePrev() {
+  decrement();
+  updateTrackPosition();
+}
+
+function handleNext() {
+  increment();
+  updateTrackPosition();
+}
+
+function updateTrackPosition() {
+  const currentIndex = getCurrentIndex();
+  const slideWidth = slides[0].offsetWidth;
+  const offset = -currentIndex * slideWidth;
+  track.style.transform = `translateX(${offset}px)`;
+}
