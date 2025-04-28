c43f75ca6ddc0f88b3dd95e4749da95053981d47
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-27 23:26:20 -0700

    Add an interval hook

---
+++ src/useAutoplay.js
@@ -0,0 +1,28 @@
+import listenAndForget from './helpers/listenAndForget';
+
+function useAutoplay(repeatable = () => {}, interval = 5000) {
+  let intervalID;
+
+  function createInterval() {
+    intervalID = setInterval(repeatable, interval);
+  }
+
+  function resetInterval() {
+    clearInterval(intervalID);
+    createInterval();
+  }
+
+  function restartAutoplay(callback) {
+    callback();
+    resetInterval();
+  }
+
+  const cleanup = listenAndForget(window, 'popstate', () => {
+    clearInterval(intervalID);
+    cleanup();
+  });
+
+  return restartAutoplay;
+}
+
+export default useAutoplay;
