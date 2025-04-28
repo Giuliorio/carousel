1f74e8929e368a9e72668725f496ffec882052c6
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-27 23:29:49 -0700

    Integrate Autoplay with rest of component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import './reset.css';
 import './styles.css';
-import useIndex from './useIndex';
+import useAutoplay from './hooks/useAutoplay';
+import useIndex from './hooks/useIndex';
 
 const slides = document.querySelectorAll('.slide');
 const track = document.querySelector('.track');
 const { getCurrentIndex, setCurrentIndex, increment, decrement } = useIndex(
   slides.length - 1
 );
+const restartAutoplay = useAutoplay(handleNext, 5000);
 
 const previousButton = document.querySelector('.previous');
 const nextButton = document.querySelector('.next');
 
-previousButton.addEventListener('click', handlePrev);
-nextButton.addEventListener('click', handleNext);
+previousButton.addEventListener('click', () => restartAutoplay(handlePrev));
+nextButton.addEventListener('click', () => restartAutoplay(handleNext));
 
 function handlePrev() {
   decrement();
