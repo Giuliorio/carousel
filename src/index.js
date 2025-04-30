e2468ff404114aa48240d117952cf599ddc3b4fc
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-29 22:12:39 -0700

    Add: a way to change autoplay options

--- src/index.js
+++ src/index.js
@@ -8,51 +8,68 @@ import image3 from './images/image3.jpg';
 import useAutoplay from './hooks/useAutoplay';
 import useIndex from './hooks/useIndex';
 import createCarouselElement from './createCarouselElement';
+import deepMerge from './helpers/deepMerge';
+
+const images = [
+  {
+    url: image1,
+    alt: 'Mountaintop Temple',
+  },
+  {
+    url: image2,
+    alt: 'Beautiful pagoda in a Japanese style town',
+  },
+  {
+    url: image3,
+    alt: 'Japanese market at night in fall',
+  },
+];
 
 const body = document.querySelector('body');
-body.appendChild(
-  createCarouselElement([
-    {
-      url: image1,
-      alt: 'Mountaintop Temple',
+
+createCarousel(body, images, {});
+
+function createCarousel(parent, images = [], options = {}) {
+  const defaultSettings = {
+    autoplay: {
+      enabled: true,
+      interval: 5000,
     },
-    {
-      url: image2,
-      alt: 'Beautiful pagoda in a Japanese style town',
-    },
-    {
-      url: image3,
-      alt: 'Japanese market at night in fall',
-    },
-  ])
-);
+  };
 
-const slides = document.querySelectorAll('.slide');
-const track = document.querySelector('.track');
-const { getCurrentIndex, setCurrentIndex, increment, decrement } = useIndex(
-  slides.length - 1
-);
-const restartAutoplay = useAutoplay(handleNext, 5000);
+  const { autoplay, interval } = deepMerge(defaultSettings, options);
 
-const previousButton = document.querySelector('.previous');
-const nextButton = document.querySelector('.next');
+  parent.appendChild(createCarouselElement(images));
 
-previousButton.addEventListener('click', () => restartAutoplay(handlePrev));
-nextButton.addEventListener('click', () => restartAutoplay(handleNext));
+  const slides = document.querySelectorAll('.slide');
+  const track = document.querySelector('.track');
+  const { getCurrentIndex, setCurrentIndex, increment, decrement } = useIndex(
+    slides.length - 1
+  );
+  const restartAutoplay = autoplay.enabled
+    ? useAutoplay(handleNext, autoplay.interval)
+    : (fn) => fn();
 
-function handlePrev() {
-  decrement();
-  updateTrackPosition();
-}
+  const previousButton = document.querySelector('.previous');
+  const nextButton = document.querySelector('.next');
 
-function handleNext() {
-  increment();
-  updateTrackPosition();
-}
+  previousButton.addEventListener('click', () => restartAutoplay(handlePrev));
+  nextButton.addEventListener('click', () => restartAutoplay(handleNext));
 
-function updateTrackPosition() {
-  const currentIndex = getCurrentIndex();
-  const slideWidth = slides[0].offsetWidth;
-  const offset = -currentIndex * slideWidth;
-  track.style.transform = `translateX(${offset}px)`;
+  function handlePrev() {
+    decrement();
+    updateTrackPosition();
+  }
+
+  function handleNext() {
+    increment();
+    updateTrackPosition();
+  }
+
+  function updateTrackPosition() {
+    const currentIndex = getCurrentIndex();
+    const slideWidth = slides[0].offsetWidth;
+    const offset = -currentIndex * slideWidth;
+    track.style.transform = `translateX(${offset}px)`;
+  }
 }
