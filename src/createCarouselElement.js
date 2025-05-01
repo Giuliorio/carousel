8641a51bfc0d1cbc2a2a1d100b2cf45cde0a2e8d
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-30 19:54:51 -0700

    Improve buttons styling

--- src/createCarouselElement.js
+++ src/createCarouselElement.js
@@ -4,11 +4,7 @@ function createCarouselElement(images = [], properties) {
   const carousel = createElement('div', {
     className: 'carousel',
     properties,
-    children: [
-      createPreviousButtonElement(),
-      createFrameElement(images),
-      createNextButtonElement(),
-    ],
+    children: [createFrameElement(images)],
   });
 
   return carousel;
@@ -17,8 +13,10 @@ function createCarouselElement(images = [], properties) {
 function createPreviousButtonElement() {
   const previousButton = createElement('button', {
     className: 'previous',
-    textContent: '‹',
   });
+
+  previousButton.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>';
 
   return previousButton;
 }
@@ -26,8 +24,10 @@ function createPreviousButtonElement() {
 function createNextButtonElement() {
   const nextButton = createElement('button', {
     className: 'next',
-    textContent: '›',
   });
+
+  nextButton.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>';
 
   return nextButton;
 }
@@ -35,7 +35,11 @@ function createNextButtonElement() {
 function createFrameElement(images) {
   const frame = createElement('div', {
     className: 'frame',
-    children: [createTrackElement(images)],
+    children: [
+      createPreviousButtonElement(),
+      createTrackElement(images),
+      createNextButtonElement(),
+    ],
   });
 
   return frame;
